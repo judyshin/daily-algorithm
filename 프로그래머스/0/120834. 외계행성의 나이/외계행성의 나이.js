37fcedc4b80d846081ec31fd98da1c2b10b5c8fc
Joohee Shin <76806400+judyshin@users.noreply.github.com>
2024-10-03 19:06:57 +0900

    Delete 프로그래머스/0/120834. 외계행성의 나이 directory

--- 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.js
+++
@@ -1,5 +0,0 @@
-function solution(num) {
-    var letters = ['a', 'b', 'c','d','e','f','g','h','i','j','k', 'l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-    var age = '' + num;
-    return [...age].map((e)=> letters[e]).join('')
-}
